9b93c57e00aae964bb4933de2fca5f69c6f9051b
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-06-02 20:59:03 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Items from "./Items";
+
+function App() {
+    return (
+      <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+          <h2>Shopping App</h2>
+        </div>
+        <Items />
+      </div>
+    );
+  }
+  
+  export default App;
